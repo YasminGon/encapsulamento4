1bf3e77d60d0e3584d763a7cc12466d1ef6c1278
YasminGon <yasmingon08@hotmail.com>
2023-05-16 13:44:05 +0000

    salvando

---
+++ Perfil.java
@@ -0,0 +1,39 @@
+public class Perfil{
+
+    private String nome;
+    private int idade;
+    private String username;
+    private String senha;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
