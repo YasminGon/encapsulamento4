74663b4fb684973520910b8534487c0d020dfa5a
YasminGon <yasmingon08@hotmail.com>
2023-05-16 14:23:53 +0000

    salvando

--- Principal.java
+++ Principal.java
@@ -1,3 +1,18 @@
 public class Principal {
-    
+    public static void main(String[] args) {
+        Perfil perfil = new Perfil("Yasmin",17,"Jomin2808","2021");
+
+        System.out.println("nome:"+perfil.getNome());
+        System.out.println("idade:"+perfil.getIdade());
+        System.out.println("Perfil:"+perfil.getUsername());
+        perfil.setSenha("Etec123");
+
+        if(perfil.verificarSenha("Etec2023")){
+            System.out.println("senha válida");
+        }else{
+            System.out.println("senha inválida!");
+        }
+
+    }
+
 }
